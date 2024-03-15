54c0fc398317ed58244df35c6521f5518e1f9997
TheArchangelWarrior <angel.bitsov@gmail.com>
2024-03-15 14:09:16 -0500

    Initial Commit

---
+++ assignmentNotebook/assignmentNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentNotebookApp.swift
+//  assignmentNotebook
+//
+//  Created by Angel Bitsov on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct assignmentNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
